344978dafcc62c626ebf69401c10577caf20a82a
dailegedai <dailegedai7302@gmail.com>
2026-05-10 21:32:26 +0800

    feat:ThreadPool and submit Implement

--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -1,6 +1,52 @@
 #include "../include/ThreadPool.h"
 #include  <iostream>
 
+ThreadPool::ThreadPool(size_t num_thread) : stop(false) {
+    for (size_t i = 0; i < num_thread; i++) {
+        workers.emplace_back([this]() {
+            std::cout << "worker thread " 
+            << std::this_thread::get_id() << "start \n";
+            while(true) {
+                std::function<void()> task;
+
+                {
+                    std::unique_lock<std::mutex> lock(this->mtx);
+
+                    std::cout << "worker waiting ...\n";
+                    this->cv.wait(lock, [this]() {
+                        return this->stop || !this->tasks.empty();
+                    });
+
+                    if (this->stop && this->tasks.empty()) {
+                        return;
+                    }
+
+                    task = this->tasks.front();
+                    this->tasks.pop();
+                }
+
+                task();
+            }
+        });
+    }
+}
+
+ThreadPool::~ThreadPool() {}
+void ThreadPool::submit(std::function<void()> task) {
+    {
+        std::lock_guard<std::mutex> lock(this->mtx);
+        this->tasks.push(move(task));
+    }
+    cv.notify_one();
+}
+
 int main() {
-    std::cout << "can run successfully\n";
+    ThreadPool pool(5);
+    for (int i = 0; i < 10; i++) {
+        pool.submit([i]() {
+            std::cout << "task" << i << std::endl;
+        });
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
 }
