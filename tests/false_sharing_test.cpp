c30d64a6bcc5f966a601789437a988469b6770c2
fangrui_32 <fangrui30@h-partners.com>
2026-07-07 23:30:30 +0800

    update false_sharing_test

--- tests/false_sharing_test.cpp
+++ tests/false_sharing_test.cpp
@@ -5,12 +5,19 @@
 #include <iostream>
 #include <thread>
 
+// true sharing cost 200~ms
 struct Counter {
     alignas(64)
     std::atomic<long> x{0};
     alignas(64)
     std::atomic<long> y{0};
 };
+
+// false sharing cost 1000~ms
+// struct Counter {
+//     std::atomic<long> x{0};
+//     std::atomic<long> y{0};
+// };
 
 Counter counter;
 
